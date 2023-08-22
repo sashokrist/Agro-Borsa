227d960930e7e7bd86bd711bc85a21ceef3f7cd9
sashokrist <alexander.krist@gmail.com>
2023-08-22 20:17:29 +0300

    fix

--- resources/views/offers/edit.blade.php
+++ resources/views/offers/edit.blade.php
@@ -84,9 +84,7 @@
 
             @endguest
         </ul>
-        <button type="button" id="newOfferBtn" class="btn btn-success" data-toggle="modal"
-                data-target="#createOfferModal">New Offer
-        </button>
+        <a href="{{ route('offers.create') }}" class="btn btn-success">New Offer</a>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
